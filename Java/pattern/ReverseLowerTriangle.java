af919f6609ecc8314f3efa5d1aaa41b460050141
YashS-2519 <y.singh2519@gmail.com>
2024-08-06 08:17:59 +0530

    day01

---
+++ Java/pattern/ReverseLowerTriangle.java
@@ -0,0 +1,14 @@
+public class ReverseLowerTriangle {
+  public static void main(String[] args) {
+
+    int length = 5;
+    
+    for (int i = 0; i < length; i++) {
+      for (int j = 0; j < i; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+
+  }
+}
---
+++ Java/pattern/ReverseUpperTriangle.java
@@ -0,0 +1,14 @@
+public class ReverseUpperTriangle {
+  public static void main(String[] args) {
+
+    int length = 5;
+    
+    for (int i = 0; i < length; i++) {
+      for (int j = 0; j < length - i; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+
+  }
+}
